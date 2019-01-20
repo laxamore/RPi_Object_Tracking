295582afe2a8a03eaa38ff7bacf08a6a9fa63a72
laxamore <mukhliskurnia60@gmail.com>
2019-01-20 20:46:37 +0700

    update

--- src/python/tracking.py
+++ src/python/tracking.py
@@ -1,9 +1,9 @@
-# #!/usr/bin/env python3
+#!/usr/bin/env python3
 
-# from picamera.array import PiRGBArray
-# from picamera import PiCamera
-# import time
-# import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
 import yaml
 import os
 
